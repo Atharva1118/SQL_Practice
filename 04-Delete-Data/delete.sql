ebea8c2fc1de7dd3125643c284df3220aa336a36
Atharva1118 <atharvag1811@gmail.com>
2026-08-27 15:29:51 +0530

    Create delete.sql with SQL delete commands
    Added SQL commands for deleting data from users table.

---
+++ 04-Delete-Data/delete.sql
@@ -0,0 +1,25 @@
+Use startersql;
+
+-- Deleting rows from a table
+-- Basic Syntax 
+-- Delete from table_name
+-- where condition;
+
+-- For creating one row
+-- Delete from users 
+-- where salary<65000;
+
+-- Delete from users
+--  where id=3;
+
+-- For deleting multiple rows
+-- Delete from users
+-- where gender='other';
+
+-- For Deleting all rows
+-- Delete from users;
+
+-- For Deleting the table
+-- Drop Table users;
+
+select * from users;
